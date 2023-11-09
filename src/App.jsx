f955d301bbf1dd1927dd2ba80255baabc1e9b3cb
Artur Pedro Martins Neto <87918748+artpedro@users.noreply.github.com>
2023-11-09 01:09:38 +0000

    Criação de card e estilização

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      app
-    </>
+  <html>
+   <div className="card">
+      <h2>Rick Sanchez</h2>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    </div>
+  </html>
   )
 }
 
